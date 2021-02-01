9a1f416550caaf2b2f366c0563815f14d3977120
minh2401 <minhtnps10422@fpt.edu.vn>
2021-02-01 12:01:02 +0700

    minhminh

--- resources/views/BE/layout/dashboard.blade.php
+++ resources/views/BE/layout/dashboard.blade.php
@@ -1,37 +1,9 @@
 @extends('BE.layout.layout')
 @section('pagetitle','Trang Thống Kê')
 @section('content')
-<div class="app-content content ">
-    <div class="content-overlay"></div>
-    <div class="header-navbar-shadow"></div>
-    <div class="content-wrapper">
-        <div class="content-header row">
-            <div class="content-header-left col-md-9 col-12 mb-2">
-                <div class="row breadcrumbs-top">
-                    <div class="col-12">
-                        <h2 class="content-header-title float-left mb-0">DashBoard</h2>
-                        <div class="breadcrumb-wrapper">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="index.html">Home</a>
-                                </li>
-                                <li class="breadcrumb-item"><a href="#">Layouts</a>
-                                </li>
-                                <li class="breadcrumb-item active">Layout Empty
-                                </li>
-                            </ol>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="content-header-right text-md-right col-md-3 col-12 d-md-block d-none">
-                <div class="form-group breadcrumb-right">
-                    <div class="dropdown">
-                        <button class="btn-icon btn btn-primary btn-round btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i data-feather="grid"></i></button>
-                        <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="app-todo.html"><i class="mr-1" data-feather="check-square"></i><span class="align-middle">Todo</span></a><a class="dropdown-item" href="app-chat.html"><i class="mr-1" data-feather="message-square"></i><span class="align-middle">Chat</span></a><a class="dropdown-item" href="app-email.html"><i class="mr-1" data-feather="mail"></i><span class="align-middle">Email</span></a><a class="dropdown-item" href="app-calendar.html"><i class="mr-1" data-feather="calendar"></i><span class="align-middle">Calendar</span></a></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+@section('br-namepage1','Trang Thống Kê')
+@section('br-namepage2','Tổng Quan')
+@section('br-namepage3','Thống kê trang web')
         <div class="content-body">
             <div class="row">
                 <div class="col-12">
@@ -45,6 +17,5 @@
             </div>
 
         </div>
-    </div>
-</div>
+
 @endsection
