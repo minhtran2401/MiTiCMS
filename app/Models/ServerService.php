116bd1bb855cbd0100828b02a6d9ca40ecf9dfd9
shin254 <57482473+shin254@users.noreply.github.com>
2021-02-02 08:37:12 +0700

    more model (2)

---
+++ app/Models/ServerService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class ServerService extends Model
+{
+    protected $table='server_service';
+    protected $primaryKey='server_id';
+    protected $fillable = [
+        'service_group_id',
+        'service_type_id',
+        'sku',
+        'server_image',
+        'server_name',
+        'server_profile',
+        'display',
+        'slug'
+    ];
+}
